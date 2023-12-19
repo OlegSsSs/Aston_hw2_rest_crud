572da2ee0c5b827b212470225b888f3173c4c937
OlegSsSs <114469359+OlegSsSs@users.noreply.github.com>
2023-12-19 19:46:00 +0300

    Update UserDto.java

--- src/main/java/dto/UserDto.java
+++ src/main/java/dto/UserDto.java
@@ -1,11 +1,14 @@
 package dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 
