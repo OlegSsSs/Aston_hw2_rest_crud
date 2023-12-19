ff1104e573ff00ba4a8f99a8f30a398c45814055
OlegSsSs <114469359+OlegSsSs@users.noreply.github.com>
2023-12-19 19:55:41 +0300

    Update UserService.java

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 public interface UserService {
     List<UserDto> getAllUsers();
+    
     UserDto getUserById(Long id);
+    
     UserDto saveUser(UserDto userDto);
+    
     UserDto updateUser(UserDto userDto);
+    
     boolean deleteUser(Long id);
 }
